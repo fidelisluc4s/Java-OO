b98e4b5c93ebf15813c61ce84943e0dcf0ba0162
Lucas Fidelis <134640628+fidelisluc4s@users.noreply.github.com>
2023-10-03 19:39:33 -0300

    Exercicio02 lista

--- ImparesDeAeB/src/Principal.java
+++ ImparesDeAeB/src/Principal.java
@@ -3,22 +3,19 @@ import java.util.ArrayList;
 
 public class Principal {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner le = new Scanner(System.in);
             
         Impares impares = new Impares();
 
-        System.out.print("Digite o valor de A: ");
-        int numeroA = scanner.nextInt();
-        impares.setNumeroA(numeroA);
+        System.out.print("Digite o valor de A: ");        
+        impares.setNumeroA(le.nextInt());
 
         System.out.print("Digite o valor de B: ");
-        int numeroB = scanner.nextInt();
-        impares.setNumeroB(numeroB);
+        
+        impares.setNumeroB(le.nextInt());
 
-        impares.oNumerosImpares();
-        impares.aNumerosImpares();
-
-        ArrayList<Integer> numerosImpares = impares.getNumerosImpares();
+        impares.obterNumerosImpares();
+        
         System.out.println("Números ímpares entre " + numeroA + " e " + numeroB + ":");
         for (int numero : numerosImpares) {
             System.out.println(numero);
