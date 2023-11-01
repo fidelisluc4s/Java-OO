7522609778d54d40fe073474dfefe517386736d9
Lucas Fidelis <134640628+fidelisluc4s@users.noreply.github.com>
2023-11-01 00:36:47 -0300

    e

--- Construtora/Condominio.java
+++ Construtora/Condominio.java
@@ -1,11 +1,48 @@
+import java.util.ArrayList;
 
 
 public class Condominio
 {
     private String nome;
     private String endereco;
+    private ArrayList<Despesa> despesas;
     
-    public Condominio(){        
+    public Condominio(){
+        this("","");      
     }
+    
+    public Condominio(String nome, String endereco){
+        this.nome = nome;
+        this.endereco = endereco;
+        despesas = new ArrayList<>();
+    }
+    
+    public double valorCondominio(int mesAno){
+        return 0;
+    }
+    
+    public void pagarCondominio(Imovel imovel, int mesAno){
+        
+    }
+    
+    public String relatorio(){
+        return "";
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+    
+    public String getEndereco(){
+        return endereco;
+    }
+    
     
 }
---
+++ Construtora/Imovel.java
@@ -0,0 +1,50 @@
+
+
+public class Imovel
+{
+    private int numero;
+    protected int metrosQuadrados;
+    private String proprietario;
+    private Condominio condominio;
+    
+    public Imovel(){
+    }
+    public Imovel(int numero,int metrosQuadrados,String proprietario){
+        this.numero = numero;
+        this.metrosQuadrados = metrosQuadrados;
+        this.proprietario = proprietario;
+    }
+    
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
+    
+    public int getNumero(){
+        return numero;
+    }
+    public void setMetrosQuadrados(int metrosQuadrados){
+        this.metrosQuadrados = metrosQuadrados;
+    }
+    
+    public int getMetrosQuadrados(){
+        return metrosQuadrados;
+    }
+    
+    public void setProprietario(String proprietario){
+        this.proprietario = proprietario;
+    }
+    
+    public String getProprietario(){
+        return proprietario;
+    }
+    
+    public void setCondominio(Condominio condominio){
+        this.condominio = condominio;
+    }
+    public Condominio getCondominio(){
+        return condominio;
+    }
+    
+    
+    
+}
